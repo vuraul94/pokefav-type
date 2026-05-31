00f6c820dcbbb8dd84915587ebb6e002753c4513
Raúl Venegas Ugalde <vuraul94@gmail.com>
2026-05-30 20:45:05 -0600

    path fix

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite';
+import pkg from './package.json';
 
-export default defineConfig({
-  base: '/pokefav-type/', 
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  return {
+    // Set the base path for deployment.
+    // For production builds, it reads the repository name from package.json.
+    // For local development, it uses the root path '/'.
+    base: command === 'build' 
+      ? `/${pkg.repositoryName}/` 
+      : '/',
+  };
 });
